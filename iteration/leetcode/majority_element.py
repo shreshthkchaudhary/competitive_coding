a649cc51420b908ed1b131d1dc0e9ced11ccf388
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-08-13 10:43:45 +0530

    adding content

--- iteration/leetcode/majority_element.py
+++ iteration/leetcode/majority_element.py
@@ -1,16 +1,31 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-        if len(nums)<3:
-            return nums[0]
-        nums.sort()
-        count=0
-        for i in range(1,len(nums)):
-            if nums[i-1]==nums[i]:
-                count+=1
-                if count>=len(nums)//2:
-                    return nums[i]
-            else:
-                count=0
+
+        # Brute Force
+        # if len(nums)<3:
+        #     return nums[0]
+        # nums.sort()
+        # count=0
+        # for i in range(1,len(nums)):
+        #     if nums[i-1]==nums[i]:
+        #         count+=1
+        #         if count>=len(nums)//2:
+        #             return nums[i]
+        #     else:
+        #         count=0
+
+
+
+        #leetcode helper
+        candidate = None
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+
+        return candidate
 
 
 result=Solution()
