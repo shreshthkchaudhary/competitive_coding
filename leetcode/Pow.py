79a05493b3c49f5c83912186273fbbe7f16aaf2a
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-09-13 23:54:25 +0530

    adding content - 13/09/2026

---
+++ leetcode/Pow.py
@@ -0,0 +1,31 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # return x**n
+        # res=1
+        # if n>0:
+        #     while n!=0:
+        #         res*=x
+        #         n-=1
+        #     return res
+        # elif n<0:
+        #     while n!=0:
+        #         res/=x
+        #         n+=1
+        #     return res
+
+        # return 1
+
+        
+
+
+
+
+
+
+
+
+
+
+
+result=Solution()
+print(result.myPow(2.10000, -3))
