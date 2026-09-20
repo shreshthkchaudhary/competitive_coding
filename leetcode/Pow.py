3bbf8f78bc5082e1a7d886e77d301ebb5a809dc6
Shreshth Kumar Chaudhary <ishu9411kumar@gmail.com>
2026-09-20 23:53:59 +0530

    adding content - 20/09/2026

--- leetcode/Pow.py
+++ leetcode/Pow.py
@@ -1,8 +1,10 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
 
-# Brute Force        
+# Direct Method
+        # return x**n
+
+# Brute Force  TDL    
         # res=1
         # if n>0:
         #     while n!=0:
@@ -18,5 +20,21 @@ class Solution:
         # return 1
 
 
+# Mathmatical Way
+        pow = n
+        if pow < 0:
+              pow *= -1
+              x = 1/x
+        ans = 1
+        while pow > 0:
+                if pow % 2 == 1:
+                      ans *= x
+                      pow -= 1
+                else:
+                        x *= x
+                        pow /= 2
+        return ans
+
+
 result=Solution()
-print(result.myPow(2.10000, -3))
+print(result.myPow(2.10000, -1))
